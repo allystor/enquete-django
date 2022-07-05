073987bb44d480935b0712f204634e65fa234304
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-07-05 20:16:23 -0300

    update commit

--- enquete/polls/views.py
+++ enquete/polls/views.py
@@ -1,18 +1,29 @@
 from django import http
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question, Choice
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Questions
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def detail(request,question_id):
-    return HttpResponse(f"Esta é a questão {question_id}.")
+    latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request,"index.html", context)
 
 def results(request,question_id):
-    response = HttpResponse(f"Este é o resultado da questão {question_id}.")
-    return response
+    response = Questions(pk=question_id)
+    return render(request, "results.html", {'response': response})
 
 def vote(request,question_id):
-    return HttpResponse(f"Você votou na questão {question_id}.")
+    question = get_object_or_404(Questions, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request,"vote.html",{
+            'question': question,
+            'error_message': "Voce não selecionou nenhuma opcao",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
